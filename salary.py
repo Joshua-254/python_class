8aaa9066a263ab3a15ea10eb1d511081af73c422
oshua-254 <hosanajoshua@gmail.com>
2022-01-24 10:50:04 +0300

    Add the hourlt gross salary pay code

---
+++ salary.py
@@ -0,0 +1,6 @@
+# python program to calculate the gross pay 
+print("Hello Welcome to the gross rate calculator \n");
+hours = float((input("Enter Hours : ")));
+rate = float((input("Enter Rate : ")));
+pay = hours * rate;
+print("Pay : ", pay);
