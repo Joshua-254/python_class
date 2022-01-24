5089d365e2e19ed1d9276c80f336b37d361c30d6
oshua-254 <hosanajoshua@gmail.com>
2022-01-24 10:49:18 +0300

    Add the class file used for learning datatypes and varables in the second class on 24th January

---
+++ class.py
@@ -0,0 +1,62 @@
+"""
+#constants
+a = 10;
+b = 20.135;
+c = "Hello World";
+
+print(a);
+print(b);
+print(c);
+"""
+
+# python operators
+d = 5;
+e = 10;
+multiplication = d * e;
+division = d/e;
+addition = d + e;
+subtraction = e - d;
+power = d**e;
+remainder = e % d;
+f = 3 + 2j;
+
+print ("Multplication result :", multiplication);
+print ("division result :", division);
+print ("addition result :", addition);
+print ("subtraction result :", subtraction);
+print ("power result :", power);
+print ("remainder result :", remainder ,"\n");
+
+# checking variables using the type()
+print("Checking the variable types \n ======")
+print ("Multiplication is of : ", type(multiplication));
+print ("Division is of : ",type(division));
+print ("F is of : ",type(f));
+
+# type conversion
+age = int(input("Enter your age "));
+age_add = age + 10;
+print ("Age added 10 is ", age_add);
+
+"""
+this is a multiple line comment in python spanning multiple lines
+
+conversion of an integer to string brings an error 
+
+"""
+
+print("Strng concatination \n");
+s = "Hello ";
+t = "World";
+
+print(s+t);
+print(s*5);
+
+""""
+Type error can easily be dealt with when looking at the error result 
+
+hours rate gross pay
+"""
+
+
+
